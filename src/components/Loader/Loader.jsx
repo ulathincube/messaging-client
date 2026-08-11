60c8de7e085e5b263bc420847fb7ded586bcbdcf
Ulathi Ncube <lathincube@gmail.com>
2026-08-11 13:10:05 +0200

    Edit: auth components

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,11 @@
-function Loader() {}
+import styles from './Loader.module.css';
+
+function Loader() {
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.loader}>... Loading ...</span>
+    </div>
+  );
+}
 
 export default Loader;
